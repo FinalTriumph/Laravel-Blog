cc9ed0ff5105535ebd577b27ddd760f3402298fa
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-08-22 16:19:00 +0000

    commit

--- config/app.php
+++ config/app.php
@@ -120,8 +120,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
-    //'log' => 'errorlog',
+    //'log' => env('APP_LOG', 'single'),
+    'log' => 'errorlog',
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
