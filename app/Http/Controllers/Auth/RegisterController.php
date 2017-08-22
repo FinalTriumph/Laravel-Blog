551584b86f5f7c4ef2c59b18a3580fd006536248
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-08-22 17:02:17 +0000

    commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'about' => null,
+            'about' => '-this_is/about/default_text-'
         ]);
     }
 }
